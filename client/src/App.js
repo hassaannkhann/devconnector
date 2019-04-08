ccd276438c504ee585d8238118e39d8a92571343
HASSAAN KHAN <kahnamite10@gmail.com>
2019-04-08 16:59:38 +0500

    Redux Authentication Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -7,9 +10,30 @@ import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 
-import "./App.css";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+
+import "./App.css";
+
+//Check For Auth Token
+if (localStorage.jwtToken) {
+  //Set Auth Token Header Auth
+  setAuthToken(localStorage.jwtToken);
+  //Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //Set User and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  //Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //Logout User
+    store.dispatch(logoutUser());
+    // TODO:Clear current User
+    // Redirect to Login
+    window.location.href = "/login";
+  }
+}
 
 class App extends Component {
   render() {
